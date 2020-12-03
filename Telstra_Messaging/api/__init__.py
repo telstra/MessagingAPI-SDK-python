c106218ef50e772f8a95d6478299e084a8991379
David Andersson <david-andersson@users.noreply.github.com>
2020-12-03 13:09:38 +1100

    remove old code

--- Telstra_Messaging/api/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import
-
-# flake8: noqa
-
-# import apis into api package
-from Telstra_Messaging.api.authentication_api import AuthenticationApi
-from Telstra_Messaging.api.messaging_api import MessagingApi
-from Telstra_Messaging.api.provisioning_api import ProvisioningApi
