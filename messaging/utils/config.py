8e764504ffe27b64f12d9aae48bb264ca50e0f43
David Andersson <david-andersson@users.noreply.github.com>
2021-01-11 15:22:54 +1100

    add configuration that reads values as needed

---
+++ messaging/utils/config.py
@@ -0,0 +1,93 @@
+"""Configuration for requests."""
+
+import os
+import typing
+
+from .. import exceptions
+
+TTlsClientKey = str
+TTlsClientSecret = str
+
+
+class Config:
+    """
+    Configuration for requests.
+
+    Attrs:
+        tls_client_key: The OAuth client id
+        tls_client_secret: The OAuth client secret
+
+    """
+
+    _tls_client_key: typing.Optional[TTlsClientKey] = None
+    _tls_client_secret: typing.Optional[TTlsClientSecret] = None
+
+    @property
+    def tls_client_key(self) -> TTlsClientKey:
+        """Get the tls_client_key."""
+        if self._tls_client_key is None:
+            tls_client_key_env_name = "TLS_CLIENT_KEY"
+            tls_client_key = os.getenv(tls_client_key_env_name)
+            if not isinstance(tls_client_key, TTlsClientKey):
+                raise exceptions.CredentialError(
+                    "The client key was not configured. "
+                    "It can be retrieved from here: "
+                    "https://dev.telstra.com/user/me/apps. "
+                    f"Then it can be provided as the '{tls_client_key_env_name}' "
+                    "environment variable or "
+                    "using the following code: \n"
+                    "from messaging.utils import CONFIG\n"
+                    "CONFIG.tls_client_key = '<client key>'"
+                )
+            self._tls_client_key = tls_client_key
+        return self._tls_client_key
+
+    @tls_client_key.setter
+    def tls_client_key(self, tls_client_key: TTlsClientKey) -> None:
+        """Set the tls_client_key."""
+        if not (isinstance(tls_client_key, TTlsClientKey)):
+            raise exceptions.CredentialError(
+                "The provided client key is not valid, "
+                f"expecting a {TTlsClientKey}, "
+                f"received {tls_client_key}."
+            )
+        self._tls_client_key = tls_client_key
+
+    @property
+    def tls_client_secret(self) -> TTlsClientSecret:
+        """Get the tls_client_secret."""
+        if self._tls_client_secret is None:
+            tls_client_secret_env_name = "TLS_CLIENT_SECRET"
+            tls_client_secret = os.getenv(tls_client_secret_env_name)
+            if not isinstance(tls_client_secret, TTlsClientSecret):
+                raise exceptions.CredentialError(
+                    "The client secret was not configured. "
+                    "It can be retrieved from here: "
+                    "https://dev.telstra.com/user/me/apps. "
+                    f"Then it can be provided as the '{tls_client_secret_env_name}'' "
+                    "environment variable or "
+                    "using the following code: \n"
+                    "from messaging.utils import CONFIG\n"
+                    "CONFIG.tls_client_secret = '<client secret>'"
+                )
+            self._tls_client_secret = tls_client_secret
+        return self._tls_client_secret
+
+    @tls_client_secret.setter
+    def tls_client_secret(self, tls_client_secret: TTlsClientSecret) -> None:
+        """Set the tls_client_secret."""
+        if not (isinstance(tls_client_secret, TTlsClientSecret)):
+            raise exceptions.CredentialError(
+                "The provided client secret is not valid, "
+                f"expecting a {TTlsClientSecret}, "
+                f"received {tls_client_secret}."
+            )
+        self._tls_client_secret = tls_client_secret
+
+
+CONFIG = Config()
+
+
+def get() -> Config:
+    """Get the configuration."""
+    return CONFIG
---
+++ tests/messaging/utils/test_config.py
@@ -0,0 +1,140 @@
+"""Tests for the configuration."""
+
+import pytest
+
+from messaging import exceptions
+from messaging.utils import config
+
+
+GET_ERROR_TESTS = [
+    pytest.param(
+        "TLS_CLIENT_KEY",
+        "tls_client_key",
+        exceptions.CredentialError,
+        [
+            "client key",
+            "https://dev.telstra.com/user/me/apps",
+        ],
+        id="TLS_CLIENT_KEY",
+    ),
+    pytest.param(
+        "TLS_CLIENT_SECRET",
+        "tls_client_secret",
+        exceptions.CredentialError,
+        [
+            "client secret",
+            "https://dev.telstra.com/user/me/apps",
+        ],
+        id="TLS_CLIENT_SECRET",
+    ),
+]
+
+
+@pytest.mark.parametrize(
+    "env_name, config_name, expected_exception, expected_contents", GET_ERROR_TESTS
+)
+@pytest.mark.config
+def test_get_error(
+    env_name, config_name, expected_exception, expected_contents, monkeypatch
+):
+    """
+    GIVEN environment variable name, configuration name and expected exception and
+        contents
+    WHEN the environment variable is deleted and the config retrieved
+    THEN the expected exception is raised with the expected contents.
+    """
+    monkeypatch.delenv(env_name)
+
+    with pytest.raises(expected_exception) as exc_info:
+        getattr(config.Config(), config_name)
+
+    for content in expected_contents:
+        assert content in str(exc_info.value)
+
+
+GET_ENV_DEFINED_TESTS = [
+    pytest.param(
+        "TLS_CLIENT_KEY",
+        "tls_client_key",
+        "client key 1",
+        id="TLS_CLIENT_KEY",
+    ),
+    pytest.param(
+        "TLS_CLIENT_SECRET",
+        "tls_client_secret",
+        "client secret 1",
+        id="TLS_CLIENT_SECRET",
+    ),
+]
+
+
+@pytest.mark.parametrize("env_name, config_name, env_value", GET_ENV_DEFINED_TESTS)
+@pytest.mark.config
+def test_get_env_defined(env_name, config_name, env_value, monkeypatch):
+    """
+    GIVEN environment variable name and value and configuration name
+    WHEN the environment variable is set and the config retrieved
+    THEN value from the environment is returned.
+    """
+    monkeypatch.setenv(env_name, env_value)
+
+    returned_value = getattr(config.Config(), config_name)
+
+    assert returned_value == env_value
+
+
+GET_CONFIG_SET_TESTS_TESTS = [
+    pytest.param("tls_client_key", "client key 1", id="tls_client_key"),
+    pytest.param("tls_client_secret", "client secret 1", id="tls_client_secret"),
+]
+
+
+@pytest.mark.parametrize("config_name, config_value", GET_CONFIG_SET_TESTS_TESTS)
+@pytest.mark.config
+def test_get_config_set(config_name, config_value):
+    """
+    GIVEN configuration name and value
+    WHEN the config is set is set and then retrieved
+    THEN value from the environment is returned.
+    """
+    config_instance = config.Config()
+    setattr(config_instance, config_name, config_value)
+
+    returned_value = getattr(config_instance, config_name)
+
+    assert returned_value == config_value
+
+
+SET_ERROR_TESTS = [
+    pytest.param(
+        "tls_client_key",
+        None,
+        exceptions.CredentialError,
+        [str(None), "str"],
+        id="tls_client_key",
+    ),
+    pytest.param(
+        "tls_client_secret",
+        None,
+        exceptions.CredentialError,
+        [str(None), "str"],
+        id="tls_client_secret",
+    ),
+]
+
+
+@pytest.mark.parametrize(
+    "config_name, config_value, expected_exception, expected_contents", SET_ERROR_TESTS
+)
+@pytest.mark.config
+def test_set_error(config_name, config_value, expected_exception, expected_contents):
+    """
+    GIVEN configuration name and value and expected exception and contents
+    WHEN the configuration is set
+    THEN the expected exception is raised with the expected contents.
+    """
+    with pytest.raises(expected_exception) as exc_info:
+        setattr(config.Config(), config_name, config_value)
+
+    for content in expected_contents:
+        assert content in str(exc_info.value)
