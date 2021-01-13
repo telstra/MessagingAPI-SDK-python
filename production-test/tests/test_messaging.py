d0e249915766d7f391a814c4b61614f5e159fdb2
David Andersson <david-andersson@users.noreply.github.com>
2021-01-14 10:56:34 +1100

    add test for retrieving the next reply

--- production-test/tests/test_messaging.py
+++ production-test/tests/test_messaging.py
@@ -11,3 +11,14 @@ def test_send_sms():
     """
     subscription_value = subscription.get()
     sms.send(to=subscription_value.destination_address, body="Test")
+
+
+def test_get_reply():
+    """
+    GIVEN credentials in the environment
+    WHEN send and then get_next_unread_reply is called
+    THEN no errors are raised.
+    """
+    subscription_value = subscription.get()
+    sms.send(to=subscription_value.destination_address, body="Test")
+    sms.get_next_unread_reply()
