d8dce8de298eb8ef84ae619458a80c0880462627
David Andersson <david-andersson@users.noreply.github.com>
2021-01-14 13:09:00 +1100

    update production tests to include retrieving the status

--- production-test/tests/test_messaging.py
+++ production-test/tests/test_messaging.py
@@ -1,6 +1,6 @@
 """Tests for the messaging API."""
 
-from tls.messaging import sms, subscription
+from tls.messaging import exceptions, sms, subscription
 
 
 def test_send_sms():
@@ -22,3 +22,21 @@ def test_get_reply():
     subscription_value = subscription.get()
     sms.send(to=subscription_value.destination_address, body="Test")
     sms.get_next_unread_reply()
+
+
+def test_get_status():
+    """
+    GIVEN credentials in the environment and a sent SMS
+    WHEN get_status is called
+    THEN no errors are raised.
+    """
+    subscription_value = subscription.get()
+    sent_sms = sms.send(to=subscription_value.destination_address, body="Test")
+
+    retries = 5
+    for retry_counter in range(retries):
+        try:
+            sms.get_status(sent_sms.message_id)
+        except exceptions.SmsError as exc:
+            if retry_counter == retries - 1:
+                raise exc
