c626ccda997285ad98e14f583c3697b79d17e6c0
ACCOUNT-01\d650261 <Anderson.Zhang@team.telstra.com>
2024-05-09 14:57:04 +1000

    update oauth

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,8 +8,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [3.2.0] - 2024-05-09
 
 - Update actions.yaml
-- Update scope
-
+- Update scope for access token issue
 
 ## [3.1.0] - 2023-06-07
 
--- telstra/messaging/oauth.py
+++ telstra/messaging/oauth.py
@@ -28,7 +28,7 @@ class TToken:
 
     """
 
-    def __init__(self, access_token: str, token_type: str, expires_in: str):
+    def __init__(self, access_token: str, token_type: str, expires_in: str, scope: str = None):
         """Construct."""
         self.retrieved_at = math.ceil(time.time())
         self.access_token = access_token
