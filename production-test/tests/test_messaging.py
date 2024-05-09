199438b8cb9e1fd17adad68d0b56c547cae6d08a
ACCOUNT-01\d650261 <Anderson.Zhang@team.telstra.com>
2024-05-09 19:42:37 +1000

    v3.3.4

--- production-test/tests/test_messaging.py
+++ production-test/tests/test_messaging.py
@@ -6,22 +6,6 @@ from telstra.messaging import free_trial_numbers, message, virtual_number
 from telstra.messaging.exceptions import MessageError, VirtualNumbersError
 
 
-def test_create_numbers():
-    """
-    GIVEN credentials in the environment
-    WHEN create, get and delete are called
-    THEN no errors are raised.
-    """
-    vn_response = None
-    try:
-        vn_response = virtual_number.assign()
-        virtual_number.get(virtual_number=vn_response.virtual_number)
-        virtual_number.delete(virtual_number=vn_response.virtual_number)
-    except VirtualNumbersError as exception_:
-        if "contact support to add more" not in str(exception_):
-            raise exception_
-
-
 def test_send_get_message():
     """
     GIVEN credentials in the environment
--- telstra/messaging/__init__.py
+++ telstra/messaging/__init__.py
@@ -1,3 +1,3 @@
 """Messaging."""
 
-__version__ = "3.3.3"
+__version__ = "3.3.4"
